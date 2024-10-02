dadf4d320cec07fad44d06f4f532d84608587874
chaitanyagunjal04 <chaitanyagunjal04@gmil.com>
2024-10-02 19:24:35 +0530

    added India

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.err.println("Hello");
+        System.err.println("Hello India");
     }
 }
